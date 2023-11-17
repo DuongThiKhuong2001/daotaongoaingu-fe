82c46640550f27f2c3b7d386788c412d742001d7
khuong301201 <duongthikhuong2001cm@gmail.com>
2023-11-17 23:15:40 +0700

    toi17.11

--- src/app/components/admin/class/detail-class/detail-class.component.ts
+++ src/app/components/admin/class/detail-class/detail-class.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { LopHoc } from 'src/app/models/LopHoc';
 
@@ -7,7 +7,8 @@ import { LopHoc } from 'src/app/models/LopHoc';
   templateUrl: './detail-class.component.html',
   styleUrls: ['./detail-class.component.css'],
 })
-export class DetailClassComponent {
+export class DetailClassComponent implements OnInit {
+  public gioHoc!: string;
   constructor(
     @Inject(MAT_DIALOG_DATA)
     public data: {
@@ -15,13 +16,28 @@ export class DetailClassComponent {
     },
     private dialogRef: MatDialogRef<DetailClassComponent>
   ) {
-    console.log(this.data.lopHoc)
+    console.log(this.data.lopHoc);
+  }
+  ngOnInit() {
+    this.xuLyKyHieuLichHoc();
   }
 
+  xuLyKyHieuLichHoc() {
+    const kiHieu = this.data.lopHoc.lichHoc?.kiHieu;
+    if (kiHieu?.includes('CASANG')) {
+      this.gioHoc = '8:00-9:30';
+    } else if (kiHieu?.includes('CACHIEU')) {
+      this.gioHoc = '14:00-15:30';
+    } else if (kiHieu?.includes('CATOI')) {
+      this.gioHoc = '18:00-19:30';
+    } else {
+      this.gioHoc = ''; // hoặc giá trị mặc định khác
+    }
+  }
   closePopup() {
     this.dialogRef.close('no');
   }
-  doiGv(){
+  doiGv() {
     this.dialogRef.close('ok');
   }
 }
